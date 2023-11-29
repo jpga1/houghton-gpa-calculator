ab12f1207a48403a507ab8c322e712a505e0988e
josh k <106359223+mga01@users.noreply.github.com>
2023-11-29 15:52:55 -0500

    Adjusted styling and set the values of expected grades

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// Make a reference to the calculate button
 const calculateButton = document.querySelector('.calculate-button')
 
+
 calculateButton.addEventListener('click', () => {
+    // Calculate the current semester credits
     const creditsEarned = document.querySelectorAll('#credits-earned')
     let totalCredits = 0
 
@@ -8,5 +11,20 @@ calculateButton.addEventListener('click', () => {
         totalCredits += Number(credit.value)
     })
 
-    console.log(totalCredits)
+    console.log("a: " + totalCredits)
+
+    // Calculate the weighted sum
+    const expectedGrades = document.querySelectorAll('#expected-grade')
+    let weightedSum = 0
+
+    // TODO: Figure out how to extract the values of credits earned and expected grades in order to calculate the weighted sum
+    expectedGrades.forEach(grade => {
+        console.log(grade.value)
+    })
+
+    // (creditsEarned, expectedGrades).forEach((credit, grade) => {
+    //     weightedSum += Number(credit.value) * Number(grade.value)
+    // })
+
+    // console.log("b: " + weightedSum)
 })
