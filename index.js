8f43073a03173967f2eb766c38a8d4b3b8581a9c
josh k <106359223+mga01@users.noreply.github.com>
2023-12-01 02:24:37 -0500

    Added add row functionality and output results to document

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// Make a reference to the calculate button
+// Make a reference to the calculate and add row buttons
 const calculateButton = document.querySelector('.calculate-button')
+const addRowButton = document.querySelector('.add-row-button')
 
 
 calculateButton.addEventListener('click', () => {
@@ -12,16 +13,98 @@ calculateButton.addEventListener('click', () => {
     let weightedSum = calculateWeightedSum(expectedGrades, creditsEarned)
 
     // Calculate current semester gpa and overall gpa
-    // console.log("current semester gpa: " + calculateCurrentSemesterGPA(weightedSum, totalCredits))
 
-    console.log("overall gpa: " + (calculateOverallGPA(weightedSum,
+    const currentSemesterGPA = calculateCurrentSemesterGPA(weightedSum, totalCredits).toFixed(2)
+    
+    const overallGPA = calculateOverallGPA(weightedSum,
         document.querySelector('#previous-cumulative-gpa').value,
         document.querySelector('#previous-cumulative-credits-earned').value,
-        totalCredits)).toFixed(2))
+        totalCredits).toFixed(2)
+    
+    outputResultsToDocument(currentSemesterGPA, overallGPA)
 
     // TODO: Optimize code
-    // TODO: Output results to page
 })
+
+function outputResultsToDocument(currentSemesterGPA, overallGPA){
+    const currentSemesterGPAOutput = document.createElement('span')
+    const overallGPAOutput = document.createElement('span')
+
+    currentSemesterGPAOutput.innerText = 'Current GPA: ' + currentSemesterGPA
+    overallGPAOutput.innerHTML = 'Overall GPA: ' + overallGPA
+
+    currentSemesterGPAOutput.style.color = 'white'
+    overallGPAOutput.style.color = 'white'
+
+    currentSemesterGPAOutput.style.fontSize = '24px'
+    overallGPAOutput.style.fontSize = '24px'
+
+    const container = document.querySelector('.container')
+
+    container.append(currentSemesterGPAOutput)
+    container.append(overallGPAOutput)
+}
+
+addRowButton.addEventListener('click', () => {
+    const subContainer = document.querySelector('.sub-container')
+    const newRow = makeNewRow()
+
+    // Add row items into the sub-container
+    newRow.forEach(rowItem => {
+        subContainer.append(rowItem)
+    })
+})
+
+function makeNewRow(){
+    const courseTitleInput = document.createElement('input')
+    courseTitleInput.type = 'text'
+    courseTitleInput.name = 'course-title'
+
+    const courseCreditInput = document.createElement('select')
+    courseCreditInput.name = 'credits-earned'
+    courseCreditInput.id = 'credits-earned'
+    
+    const courseCreditInputDefaultOption = document.createElement('option')
+    courseCreditInputDefaultOption.selected
+
+    courseCreditInput.append(courseCreditInputDefaultOption)
+
+    for(let i = 1.0; i < 5; i++){
+        courseCreditInput.append(makeNewOption(i.toFixed(1), i.toFixed(1)))
+    }
+
+    const expectedGradeInput = document.createElement('select')
+    expectedGradeInput.name = 'expected-grade'
+    expectedGradeInput.id = 'expected-grade'
+
+    const expectedGradeInputDefaultOption = document.createElement('option')
+    expectedGradeInputDefaultOption.selected
+
+    expectedGradeInput.append(expectedGradeInputDefaultOption)
+
+    expectedGradeInput.append(makeNewOption(4.0, 'A'))
+    expectedGradeInput.append(makeNewOption(3.67, 'A-'))
+    expectedGradeInput.append(makeNewOption(3.33, 'B+'))
+    expectedGradeInput.append(makeNewOption(3.0, 'B'))
+    expectedGradeInput.append(makeNewOption(2.67, 'B-'))
+    expectedGradeInput.append(makeNewOption(2.33, 'C+'))
+    expectedGradeInput.append(makeNewOption(2.0, 'C'))
+    expectedGradeInput.append(makeNewOption(1.67, 'C-'))
+    expectedGradeInput.append(makeNewOption(1.33, 'D+'))
+    expectedGradeInput.append(makeNewOption(1.0, 'D'))
+    expectedGradeInput.append(makeNewOption(0.67, 'D-'))
+    expectedGradeInput.append(makeNewOption(0, 'F'))
+
+    return [courseTitleInput, courseCreditInput, expectedGradeInput]
+}
+
+function makeNewOption(value, text){
+    const newOption = document.createElement('option')
+    newOption.value = value
+    newOption.innerText = text
+
+    return newOption
+}
 
 function calculateCurrentSemesterCredits(creditsEarned){
     let totalCredits = 0
